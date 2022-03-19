e5a7f8a3e73080e19f3b43b8bafa079ed61d7cc9
Hassan Tariq <66930201+hassanmt96@users.noreply.github.com>
2022-03-18 19:32:21 -0500

    create the import requests and define the main function

---
+++ nlp_summarize.py
@@ -0,0 +1,14 @@
+from collections import Counter
+import re
+import requests
+import bs4
+import nltk
+from nltk.corpus import stop_words
+
+
+def main():
+    url = 'http://www.analytictech.com/mb021/mlk.htm'
+    page = requests.get(url)
+    page.raise_for_status()
+    soup = bs4.BeautifulSoup(page.text, 'html.parser')
+    p_elems = [element.text for element in soup.find_all('p')]
